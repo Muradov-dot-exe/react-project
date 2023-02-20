efd4f6db635b6c3ceed40408e940dd4e1c4acf22
Ali Muradov <amuradov239@gmail.com>
2023-02-20 18:01:19 +0200

    transformed the app a bit

--- react-jsx/src/index.tsx
+++ react-jsx/src/index.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const element = document.getElementById("root")!;
 
 const root = ReactDOM.createRoot(element);
-
-function App() {
-  const inputType = "number";
-  const minVal = 5;
-
-  return <textarea autoFocus={true} />;
-}
 
 root.render(<App />);
